e4e63159fa39838e13c6844a60c83b0d20f0bfa2
Julien Debrauwer <ju.debrauwer@gmail.com>
2026-02-23 00:11:02 +0100

    feat: test pokemon cached API request

---
+++ app/composables/useCapitalize.ts
@@ -0,0 +1,3 @@
+export function useCapitalize(text: string) {
+  return capitalizeText(text);
+}
--- server/api/pokemons.get.ts
+++ server/api/pokemons.get.ts
@@ -1,26 +1,20 @@
-export default defineCachedEventHandler(
-  async (event) => {
-    console.log(
-      "Fetching depuis l'API externe (tu ne devrais voir ça qu'une fois !)",
-    );
+import { capitalizeText } from "~/utils/capitalizeText";
 
-    // 1. Récupération de la liste de base
+export default defineCachedEventHandler(
+  async () => {
     const response: any = await $fetch(
       "https://pokeapi.co/api/v2/pokemon?limit=151",
     );
 
-    // 2. On récupère les détails de TOUS les Pokémon en parallèle
-    // C'est lourd, mais grâce au cache, on ne le fera qu'une seule fois !
     const detailedPokemons = await Promise.all(
       response.results.map(async (pokemon: any) => {
         const details: any = await $fetch(pokemon.url);
 
         return {
           id: details.id,
-          name: details.name.charAt(0).toUpperCase() + details.name.slice(1),
+          name: capitalizeText(details.name),
           image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${details.id}.png`,
-          // On extrait un tableau simple des types (ex: ["grass", "poison"])
-          types: details.types.map((t: any) => t.type.name),
+          types: details.types.map((typeDetail: any) => typeDetail.type.name),
         };
       }),
     );
@@ -28,8 +22,8 @@ export default defineCachedEventHandler(
     return detailedPokemons;
   },
   {
-    // CONFIGURATION DU CACHE NITRO ⚡️
-    maxAge: 60 * 60 * 24, // Garde le résultat en mémoire pendant 24 heures
-    name: "pokemons-list-cache", // Nom de la clé de cache
+    // NITRO CACHE ⚡️
+    maxAge: 60 * 60 * 24, // 24 hours
+    name: "pokemons-list-cache", // cache key
   },
 );
