5becf183a5f881174089631ec6dabbb454431bce
Julien Debrauwer <ju.debrauwer@gmail.com>
2026-02-23 00:10:16 +0100

    feat: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+      ],
+    },
+  },
   css: ["~/assets/css/main.css"],
   modules: ["shadcn-nuxt"],
   vite: {
