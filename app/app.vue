86ee3a0282f3175ede048c419f0e5a89a0da1ccd
Julien Debrauwer <ju.debrauwer@gmail.com>
2026-03-08 23:06:53 +0100

    feat: header and footer

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    <header class="p-[16px]">
-      <div class="flex items-center gap-[10px]">
+  <div class="flex flex-col h-screen overflow-hidden">
+    <header class="container py-[8px] flex items-center justify-between shrink-0">
+      <NuxtLink to="/" class="flex items-center gap-[10px]">
         <img src="/logo.svg" alt="PokeNexus" class="w-[34px] h-[34px]" />
         <h1 class="text-[24px] font-bold">PokeNexus</h1>
-      </div>
+      </NuxtLink>
+      <NuxtLink to="/pokemons" class="flex items-center gap-[10px]">
+        Pokemons
+      </NuxtLink>
     </header>
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+    <main class="flex-1 min-h-0 flex flex-col overflow-hidden">
+      <NuxtPage />
+    </main>
+    <footer class="py-[8px] shrink-0">
+    <div class="container flex items-center justify-between">
+      <a
+        href="https://github.com/Julien-Debrauwer/PokeNexus"
+        target="_blank"
+        class="underline"
+      >
+        Github
+      </a>
+      <p class="text-sm text-center">
+        &copy; {{ new Date().getFullYear() }} PokeNexus. Tous droits réservés.
+      </p>
+    </div>
+    </footer>
   </div>
 </template>
